437996d3cb667e4a866996154ae20c93ab30e45e
Sean Ackels <pyrosnowman24@gmail.com>
2021-09-02 15:52:10 -0500

    rewrote data_set_processing

--- data_set_processing.py
+++ data_set_processing.py
@@ -1,3 +1,4 @@
+from re import X
 import numpy as np
 import pandas as pd
 import os
@@ -8,59 +9,217 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import pathlib
+import glob
+from sklearn import metrics
+from tensorflow.keras.callbacks import ModelCheckpoint
 
-# Import world map
-current_path = pathlib.Path().resolve()
-folder_path = 'Map_Dataset_Generator/Datasets'
-dataset_name = 'Thu 05 Aug 2021 03:08:35 PM '
-map_name = 'Map'
-file_path = os.path.join(current_path,folder_path)
-dataset_path = os.path.join(file_path,dataset_name)
 
-map_path = os.path.join(dataset_path,map_name)
-world_img = Image.open(map_path)
+class RSU_Placement_CNN():
+    def __init__(self,train_test_split = .7):
+        self.train_test_split = train_test_split
 
-# Create transforms class
-bbox =  -98.5149, 29.4441, -98.4734, 29.3876 # San Antonio Downtown
-data_size = [250,500]
-transforms = Dataset_Transformation(bbox,world_img.size,data_size)
+        # Set all file paths
+        current_path = pathlib.Path().resolve()
+        folder_path = 'Map_Dataset_Generator/Datasets'
+        dataset_name = 'Thu 26 Aug 2021 03:29:43 PM '
+        map_name = 'Map'
+        file_path = os.path.join(current_path,folder_path)
+        self.dataset_path = os.path.join(file_path,dataset_name)
+        map_path = os.path.join(self.dataset_path,map_name)
+        data_name = "data.csv"
+        data_file_path = os.path.join(self.dataset_path,data_name)
+        images_folder = 'Images/samples'
+        images_path = os.path.join(self.dataset_path,images_folder)
 
-# Import data.csv file
-data_name = "data.csv"
-data_file_path = os.path.join(dataset_path,data_name)
-data = pd.read_csv(data_file_path)
-data_array = data.to_numpy()
+        # Import world map
+        self.world_img = Image.open(map_path)
 
-data_array = transforms.prepare_dataset(data_array)
+        # Create transforms class
+        self.bbox =  -98.5149, 29.4441, -98.4734, 29.3876 # San Antonio Downtown
+        self.data_size = [250,500]
+        self.transforms = Dataset_Transformation(self.bbox,self.world_img.size,self.data_size)
 
-# Import images
-images_folder = 'Images'
-images_path = os.path.join(dataset_path,images_folder)
-datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1.0/255.0)
-train_it = datagen.flow_from_directory(images_path, classes = None, color_mode='rgb', target_size=data_size, batch_size=64,save_format="png")
+        # Import data.csv file
+        self.data = pd.read_csv(data_file_path)
+        self.data_array = self.data.to_numpy()
+        self.data_array = self.transforms.prepare_dataset(self.data_array,scale_output = True)
+        self.df = pd.DataFrame(self.data_array[:,-2:],columns = ['x','y'])
 
-# Convert coordinates to correct format
+        # Import images
+        
+        files = glob.glob(os.path.join(images_path, "*.%s" % 'png'))
+        self.df['file'] = files
 
-# define cnn model
-def define_model():
-    model = keras.Sequential()
-    model.add(layers.Conv2D(filters = 32, kernel_size = (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(*data_size, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Conv2D(filters = 64, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Conv2D(filters = 128, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(128, activation='relu', kernel_initializer='he_uniform'))
-    model.add(layers.Dropout(.5))
-    model.add(layers.Dense(1, activation='sigmoid'))
-    # compile model
-    opt = keras.optimizers.SGD(lr=0.001, momentum=0.9)
-    model.compile(optimizer=opt, loss='mean_squared_error', metrics=['accuracy'])
-    return model
+    def __call__(self, epochs, init_lr = 1e-4):
+        model = self.assemble_full_model(*self.data_size,*self.data_size)
 
-model = define_model()
-history = model.fit_generator(train_it,data_array[:,12:],epochs=50,verbose=1)
+        train_idx, valid_idx, test_idx = self.generate_split_indexes() 
+        batch_size = 32
+        valid_batch_size = 32
+        train_gen = self.generate_images(train_idx, batch_size=batch_size)
+        valid_gen = self.generate_images(valid_idx, batch_size=valid_batch_size)
+        opt = keras.optimizers.Adam(lr=init_lr, decay=init_lr / epochs)
+        model.compile(optimizer=opt, 
+                    loss={
+                        'x_output': 'mse', 
+                        'y_output': 'mse'},
+                    metrics={
+                        'x_output': 'mae', 
+                        'y_output': 'mae'})
+
+        model_path = os.path.join(self.dataset_path,"model_checkpoint")
+        callbacks = [
+        ModelCheckpoint(model_path, monitor='val_loss')
+        ]
+
+        history = model.fit(train_gen,
+                            steps_per_epoch=len(train_idx)//batch_size,
+                            epochs=epochs,
+                            callbacks=callbacks,
+                            validation_data=valid_gen,
+                            validation_steps=len(valid_idx)//valid_batch_size)
+        self.plot_history(history,fig_path = model_path)
+        self.evaluate_model(model,test_idx)
+        plt.show()
+
+    def evaluate_model(self,model,test_idx):
+        test_batch_size = len(test_idx)
+        test_generator = self.generate_images(test_idx, batch_size=test_batch_size, is_training=False)
+        x_pred, y_pred = model.predict(test_generator, steps=len(test_idx)//test_batch_size)
+        test_generator = self.generate_images(test_idx, batch_size=test_batch_size, is_training=False)
+        images, x_true, y_true = [], [], []
+        for test_batch in test_generator:
+            image = test_batch[0]
+            labels = test_batch[1]
+            images.extend(image)
+            x_true.extend(labels[0])
+            y_true.extend(labels[1])
+        x_explained_variance = metrics.explained_variance_score(x_true,x_pred) # 1 is best, lower is worse
+        y_explained_variance = metrics.explained_variance_score(y_true,y_pred)
+        x_mae = metrics.mean_absolute_error(x_true,x_pred) # Lower is better
+        y_mae = metrics.mean_absolute_error(y_true,y_pred)
+        x_r2 = metrics.r2_score(x_true,x_pred) # 0 is the best
+        y_r2 = metrics.r2_score(y_true,y_pred)
+        print(x_explained_variance,y_explained_variance)
+        print(x_mae,y_mae)
+        print(x_r2,y_r2)
+
+    def plot_history(self,history,fig_path = None):
+        fig, (ax1,ax2) = plt.subplots(2,figsize=(15,10))
+        ax1.plot(history.history['x_output_mae'])
+        ax1.plot(history.history['val_x_output_mae'])
+        ax1.legend(["Train","Validate"])
+        ax1.set_ylabel("Mean Absolute Error")
+        ax1.set_xlabel("Epochs")
+        ax1.set_title("X Position")
+
+        ax2.plot(history.history['y_output_mae'])
+        ax2.plot(history.history['val_y_output_mae'])
+        ax2.legend(["Train","Validate"])
+        ax2.set_ylabel("Mean Absolute Error")
+        ax2.set_xlabel("Epochs")
+        ax2.set_title("Y Position")
+        
+        if fig_path is not None:
+            fig_name = os.path.join(fig_path,"train_history.png")
+            plt.savefig(fig_name)
+
+        plt.draw()
+
+    def generate_split_indexes(self):
+        p = np.random.permutation(len(self.df))
+        train_up_to = int(len(self.df) * self.train_test_split)
+        train_idx = p[:train_up_to]
+        test_idx = p[train_up_to:]
+        train_up_to = int(train_up_to * self.train_test_split)
+        train_idx, valid_idx = train_idx[:train_up_to], train_idx[train_up_to:]
+        return train_idx, valid_idx, test_idx
+
+    def preprocess_image(self, img_path):
+        im = Image.open(img_path)
+        im = im.resize((self.data_size[0], self.data_size[1]))
+        im = np.array(im) / 255.0
+        return im
+
+    def generate_images(self, image_idx, batch_size=32, is_training=True):
+        """
+        Used to generate a batch with images when training/testing/validating our Keras model.
+        """
+        # arrays to store our batched data
+        images, x_array, y_array, = [], [], []
+        while True:
+            for idx in image_idx:
+                sample = self.df.iloc[idx]
+                
+                x_coord = sample['x']
+                y_coord = sample['y']
+                file = sample['file']
+                im = self.preprocess_image(file)
+                
+                x_array.append(x_coord)
+                y_array.append(y_coord)
+                images.append(im)
+                
+                # yielding condition
+                if len(images) >= batch_size:
+                    yield np.array(images), [np.array(x_array), np.array(y_array)]
+                    images, x_array, y_array, = [], [], []
+            if not is_training:
+                break
+
+    def make_default_hidden_layers(self, inputs):
+        x = layers.Conv2D(16, (3, 3), padding="same")(inputs)
+        x = layers.Activation("relu")(x)
+        x = layers.BatchNormalization(axis=-1)(x)
+        x = layers.MaxPooling2D(pool_size=(3, 3))(x)
+        x = layers.Dropout(0.25)(x)
+
+        # x = layers.Conv2D(32, (3, 3), padding="same")(x)
+        # x = layers.Activation("relu")(x)
+        # x = layers.BatchNormalization(axis=-1)(x)
+        # x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+        # x = layers.Dropout(0.25)(x)
+
+        x = layers.Conv2D(32, (3, 3), padding="same")(x)
+        x = layers.Activation("relu")(x)
+        x = layers.BatchNormalization(axis=-1)(x)
+        x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+        x = layers.Dropout(0.25)(x)
+        return x
+
+    def build_x_branch(self, inputs, num_x):
+        x = self.make_default_hidden_layers(inputs)
+        x = layers.Flatten()(x)
+        x = layers.Dense(64)(x)
+        x = layers.Activation("relu")(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(1)(x)
+        x = layers.Activation("tanh", name="x_output")(x)
+        return x
+
+    def build_y_branch(self, inputs, num_y):
+        x = self.make_default_hidden_layers(inputs)
+        x = layers.Flatten()(x)
+        x = layers.Dense(64)(x)
+        x = layers.Activation("relu")(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(1)(x)
+        x = layers.Activation("tanh", name="y_output")(x)
+        return x
+
+    def assemble_full_model(self, width, height, num_x,num_y):
+        input_shape = (height, width, 4)
+        inputs = layers.Input(shape=input_shape)
+        x_branch = self.build_x_branch(inputs, num_x)
+        y_branch = self.build_y_branch(inputs, num_y)
+        model = keras.models.Model(inputs=inputs,
+                     outputs = [x_branch, y_branch],
+                     name="rsu_placement_net")
+        return model
+
+init_lr = 5e-2
+
+cnn = RSU_Placement_CNN()
+cnn(200)
